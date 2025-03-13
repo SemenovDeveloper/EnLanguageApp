6a16239a68c321e78c3ac8dd7dd23be0338fb86a
SemenovDev <semenovdev@yahoo.com>
2025-03-13 15:43:59 +0600

    view binding

--- app/src/main/java/com/example/enlanguageapp/MainActivity.kt
+++ app/src/main/java/com/example/enlanguageapp/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.enlanguageapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import com.example.enlanguageapp.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityLearnWordBinding
+
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding get() = _binding?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_learn_word)
+
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // нейтральный
+        // корректный
+        // некорректный
+
+
+
+
     }
 }
